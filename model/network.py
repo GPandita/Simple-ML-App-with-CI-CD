9f6c6a7d2f4983d83b8d728c850d84348bd7a405
GPandita <gauravpandita98@gmail.com>
2024-11-29 15:50:18 +0530

    Update network.py

--- model/network.py
+++ model/network.py
@@ -4,37 +4,37 @@ import torch.nn as nn
 class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
-        # First convolutional block with increased filters
+        # First convolutional block with reduced filters
         self.conv1 = nn.Sequential(
-            nn.Conv2d(1, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
             nn.LeakyReLU(0.1),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
             nn.LeakyReLU(0.1),
             nn.MaxPool2d(2, 2)
         )
         
         # Second convolutional block
         self.conv2 = nn.Sequential(
-            nn.Conv2d(64, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
             nn.LeakyReLU(0.1),
             nn.MaxPool2d(2, 2)
         )
         
-        # Fully connected layers
+        # Fully connected layers with reduced size
         self.fc = nn.Sequential(
-            nn.Linear(32 * 7 * 7, 128),
-            nn.BatchNorm1d(128),
+            nn.Linear(16 * 7 * 7, 64),  # Reduced from 128 to 64
+            nn.BatchNorm1d(64),
             nn.LeakyReLU(0.1),
             nn.Dropout(0.2),
-            nn.Linear(128, 10)
+            nn.Linear(64, 10)
         )
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
-        x = x.view(-1, 32 * 7 * 7)
+        x = x.view(-1, 16 * 7 * 7)
         x = self.fc(x)
         return x 
