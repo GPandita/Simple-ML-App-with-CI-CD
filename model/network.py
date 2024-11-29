e797517d4917472e35166fb5ca4e4742ca1b42de
GPandita <gauravpandita98@gmail.com>
2024-11-29 15:45:13 +0530

    model parameters increased

--- model/network.py
+++ model/network.py
@@ -6,35 +6,35 @@ class SimpleCNN(nn.Module):
         super(SimpleCNN, self).__init__()
         # First convolutional block with increased filters
         self.conv1 = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(1, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.LeakyReLU(0.1),
-            nn.Conv2d(32, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.LeakyReLU(0.1),
             nn.MaxPool2d(2, 2)
         )
         
         # Second convolutional block
         self.conv2 = nn.Sequential(
-            nn.Conv2d(32, 16, kernel_size=3, padding=1),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(64, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
             nn.LeakyReLU(0.1),
             nn.MaxPool2d(2, 2)
         )
         
         # Fully connected layers
         self.fc = nn.Sequential(
-            nn.Linear(16 * 7 * 7, 32),
-            nn.BatchNorm1d(32),
+            nn.Linear(32 * 7 * 7, 128),
+            nn.BatchNorm1d(128),
             nn.LeakyReLU(0.1),
-            nn.Dropout(0.2),  # Reduced dropout for faster convergence
-            nn.Linear(32, 10)
+            nn.Dropout(0.2),
+            nn.Linear(128, 10)
         )
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
-        x = x.view(-1, 16 * 7 * 7)
+        x = x.view(-1, 32 * 7 * 7)
         x = self.fc(x)
         return x 
